04c442ca84bc23d237d36c151188d7bbc37b707a
Court <courtney.lee.osborn@gmail.com>
2016-10-27 08:04:51 +0000

    Modified_1 directory.rb

--- directory.rb
+++ directory.rb
@@ -1,77 +1,47 @@
-def input_students 
+def input_students
+    
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    # create and empty array
+    # create an empty array
     students = []
-    # get the first name 
-    name = gets.chomp
-    # while the name is not empty, repeat this code 
+    # get the first name
+    name = gets.chomp.capitalize 
+    # while the name is not empty, repeat this code
     while !name.empty? do 
-        # add student hash to the array
+        # add the student hash to the array
         students << {name: name, cohort: :november}
         puts "Now we have #{students.count} students"
         # get another name from the user 
-        name = gets.chomp
+        name = gets.chomp 
     end 
     # return the array of students 
-    students
+    students 
 end 
 
-def print_header 
-    puts "The students of Villians Academy"
-    puts "-------------"
+def print_header
+    puts "The students of Villains Academy"
+    puts "--------------"
 end 
 
 def print(students)
     students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)" 
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end 
 end 
 
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students" 
+def print_footer(students)
+    puts "Overall, we have #{students.count} great students"
+end 
+
+# 1. How can you modify the program to print a number before the name of each student, e.g. "1. Dr. Hannibal Lecter"? 
+def numbering_students(students)
+    students.each_with_index {|student, index| puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"}
 end 
 
 students = input_students 
+# nothing happens until we call these methods 
 print_header
 print(students)
 print_footer(students)
-
-=begin 
-
-# let's put all students into an array
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader",  cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Micheal Corleone",  cohort: :november},
-    {name: "Alex Delarge", cohort: :november},
-    {name: "The Wicked Witch of the West",  cohort: :november},
-    {name: "Terminator",  cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker",  cohort: :november},
-    {name: "Joffrey Baratheon",  cohort: :november},
-    {name: "Norman Bates",  cohort: :november}
-]
-
-
-def print_header 
-    puts "The students of Villians Academy"
-    puts "-------------"
-end 
-
-def print(students)
-    students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)" 
-    end 
-end 
-
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students" 
-end 
-
-print_header
-print(students)
-print_footer(students)
-
-=end 
+numbering_students(students)
+    
