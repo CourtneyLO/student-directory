79417987239e940d9ec60255590e546f321b1e36
Court <courtney.lee.osborn@gmail.com>
2016-10-28 09:18:15 +0000

    Modified(12) directory_9-14.rb

--- directory_9_14.rb
+++ directory_9_14.rb
@@ -4,6 +4,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save students"
+    puts "4. Load students"
     puts "9. Exit" # 9 because we will be adding more items 
 end 
 
@@ -28,6 +29,8 @@ def process(selection)
             show_students
         when "3"
             save_students
+        when "4"
+            load_students
         when "9"
             exit # this will cause the program to terminate 
         else 
@@ -83,6 +86,15 @@ def save_students
     file.close
 end 
 
+def load_students
+    file = File.open("students.csv", 'r')
+    file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+        @students << {name: name, cohort: cohort.to_sym}
+    end 
+    file.close
+end 
+        
 
 interactive_menu
         
