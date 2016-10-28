230abf5e447421cbe79b9a76df53a08a39b29e18
Court <courtney.lee.osborn@gmail.com>
2016-10-28 10:09:10 +0000

    Modified(14.4) directory_14.rb

--- directory_14.rb
+++ directory_14.rb
@@ -1,92 +1,118 @@
 @students = []
 
-def load_students
-    filename = ARGV.first
-    return if filename.nil? 
-    if File.exists?(filename)
-        load_students(filename); puts "Loaded #{@students.count} from #{filename}"
-    else 
-        puts "Sorry, #{filename} doesn't exists"; exit
-    end 
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save students"
+    puts "4. Load students"
+    puts "9. Exit" # 9 because we will be adding more items 
 end 
 
-def load_input_students(filename = "student.csv")
-   
-    file = File.open(filename, "r")
-    file.readlines.each{|line| name, cohort = line.chomp.split(","); @students << {name: name, cohort: cohort.to_sym}}
-    
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-    name = $stdin.gets.chomp
-   
-    while !name.empty? do 
-        @students << {name: name, cohort: :november,}
-        puts "Now we have #{@students.count} students"
-        name =  $stdin.gets.chomp
-    end 
-    
-end     
-  
-
-def save_students
-    
-    file = File.open("student.csv", 'w')
-    @students.each {|student| student_data = [student[:name], student[:cohort]]; csv_line = student_data.join(","); file.puts csv_line} # 6
-    puts "Students saved" 
+def show_students
+    print_header
+    print_students_list(@students)
+    print_footer(@students)
 end 
 
 def interactive_menu
-    
     loop do 
        print_menu
-       menu_selection($stdin.gets.chomp)
+       process(STDIN.gets.chomp)
     end 
 end 
 
-def print_menu
-     # 1. print the menu and ask the user what to do
-        puts "1. load_input_students"
-        puts "2. Show the students"
-        puts "3. Save list to students.csv"
-        puts "4. Load the list from students.csv"
-        puts "9. Exit" #9 because you will be adding mor items
-end
-
-def menu_selection(selection)
+def process(selection)
     case selection
         when "1"
-            load_input_students
+            input_students
         when "2"
             show_students
         when "3"
             save_students
+        when "4"
+            load_students
         when "9"
-            exit
+            exit # this will cause the program to terminate 
         else 
             puts "I don't know what you mean, try again"
     end 
 end 
-        
-def show_students
-        print_header
-        print_student_list
-        print_footer
+
+def input_students
+    puts "Please enter the name of the students"
+    puts "To finish, just hit return twice"
+    # create an empty array 
+    @students = []
+    # get the first name 
+    name = STDIN.gets.chomp.capitalize
+    # while name is not empty, repeat this code
+    while !name.empty? do
+    # add the students has to the array
+    @students << {name: name, cohort: :november}
+    puts @students.index({name: name, cohort: :november}) > 0 ? "Now we have #{@students.count} students" : "Now we have #{@students.count} student"
+    # get another name from the user 
+    name = STDIN.gets.chomp.capitalize 
+    end
+    
+    # return array of students 
+    puts @students.count > 1 ? "#{@students.count} students have been added to the student list" : "#{@students.count} student has been added to the student list"
+    @students
+    
 end 
 
 def print_header 
-    puts "The students of Villians Academy"
+    puts "The students of the Villians Academy"
     puts "-------------"
 end 
 
-def print_student_list
+def print_students_list(students)
+    puts @students.count > 1 ? "Students:" : "Student:"
     @students.each do |student|
         puts "#{student[:name]}, (#{student[:cohort]} cohort)"
     end 
 end 
 
-def print_footer
-    puts "Overall, we have #{@students.count} great students" 
+def print_footer(students)
+    puts @students.count > 1 ? "Overall, we have #{@students.count} great students" : "Overall, we have #{@students.count} great student" 
 end 
 
-load_students
+
+def save_students
+    # open the file for writing 
+    file = File.open("students.csv", 'w')
+    # iterate over the array of students
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end 
+    puts @students.count > 1 ? "#{@students.count} students have been saved to the student list" : "#{@students.count} student has been saved to the student list" 
+    file.close
+end 
+
+def load_students(filename = "students.csv")
+    file = File.open("students.csv", 'r')
+    file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+        @students << {name: name, cohort: cohort.to_sym}
+    end 
+    puts @students.count > 1 ? "#{@students.count} students have been loaded" : "#{@students.count} student has been loaded"
+    file.close
+end 
+
+def try_load_students
+    filename = ARGV.first # first argument from the command line 
+    return if filename.nil? # get out of the method if it isn't given 
+    if File.exist?(filename)
+        load_students(filename)
+        puts "Loaded #{@students.count} from #{filename}"
+    else 
+        puts "Sorry, #{filename} doesn't exist."
+        exit # quit the program 
+    end 
+end 
+
+try_load_students
 interactive_menu
+        
+        
